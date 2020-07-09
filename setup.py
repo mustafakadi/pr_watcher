1fc5c647c5bc49e6c8d93b46623caf96acc5dc91
mustafakadi <mustafakadi25@gmail.com>
2020-07-10 00:29:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="pr_watcher",
     version="1.0.0",
-    author="Mustafa Kadıoğlu",
+    author="mustafakadi",
     author_email="mustafakadi25@hotmail.com",
     description="An app for checking PR statuses",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/mustafakadi/pr_watcher.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
